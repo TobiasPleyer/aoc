36772676796b8596ddbb120ee0e4fa898335236a
TobiasPleyer <tobi.pleyer@gmail.com>
2017-12-22 12:21:23 +0100

    Day 9, Task 2

--- Day9/day9.hs
+++ Day9/day9.hs
@@ -16,23 +16,23 @@ data ParseState = PS
   , input :: String
   }
 
-parseGroup :: Int -> Int -> [Char] -> Int
-parseGroup score nesting []
+parseGroup :: Int -> Int -> Int -> [Char] -> (Int,Int)
+parseGroup score garbage nesting []
   | nesting > 0 = error "Outermost group not closed"
-  | otherwise = score
-parseGroup score nesting (c:cs) = case c of
-  '{' -> parseGroup score (nesting+1) cs
-  '}' -> parseGroup (score+nesting) (nesting-1) cs
-  '<' -> parseGarbage score nesting cs
-  _   -> parseGroup score nesting cs
+  | otherwise = (score, garbage)
+parseGroup score garbage nesting (c:cs) = case c of
+  '{' -> parseGroup score garbage (nesting+1) cs
+  '}' -> parseGroup (score+nesting) garbage (nesting-1) cs
+  '<' -> parseGarbage score garbage nesting cs
+  _   -> parseGroup score garbage nesting cs
 
-parseGarbage :: Int -> Int -> [Char] -> Int
-parseGarbage score nesting [] = error "Garbage at the end of input"
-parseGarbage score nesting (c:cs) = case c of
-  '>' -> parseGroup score nesting cs
-  '!' -> parseGarbage score nesting (tail cs)
-  _   -> parseGarbage score nesting cs
+parseGarbage :: Int -> Int -> Int -> [Char] -> (Int,Int)
+parseGarbage score garbage nesting [] = error "Garbage at the end of input"
+parseGarbage score garbage nesting (c:cs) = case c of
+  '>' -> parseGroup score garbage nesting cs
+  '!' -> parseGarbage score garbage nesting (tail cs)
+  _   -> parseGarbage score (garbage+1) nesting cs
 
 main = do
   content <- (readFile . head) =<< getArgs
-  print $ parseGroup 0 0 content
+  print $ parseGroup 0 0 0 content
